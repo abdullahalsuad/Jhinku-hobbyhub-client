dd689b105aa874c2038c7f92f4f31e01ece7da91
Abdullah Al Suad <suadabdullahal@gmail.com>
2025-05-19 19:28:12 +0600

    projects setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# 🚀Jhinku Hobby Hub
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      Jhinku React is a React Boilerplate
+      <Outlet />
+    </>
+  );
+}
+
+export default App;
