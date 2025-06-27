dd8719f98129704129cd08c7a73e9c4311982b29
Abdullah Al Suad <suadabdullahal@gmail.com>
2025-06-27 12:35:01 +0600

    fix: remove redundant background color class from ReviewTestimonial section

--- src/components/About/ReviewTestimonial.jsx
+++ src/components/About/ReviewTestimonial.jsx
@@ -54,7 +54,7 @@ const testimonials = [
 // Main Testimonial Section
 const ReviewTestimonial = () => {
   return (
-    <section className="py-20 bg-white ">
+    <section className="py-20  ">
       <div>
         <div className="text-center mb-16">
           <h2 className="text-4xl font-bold text-gray-900  mb-4">
